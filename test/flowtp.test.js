09ea142f404cf271c60015f8e62307a285ab48c3
Braden Jonsson <braden337@gmail.com>
2017-12-15 07:28:40 -0600

    made lots of improvements

---
+++ test/flowtp.test.js
@@ -0,0 +1,17 @@
+const Flowtp = require('../src/flowtp')
+const b32 = require('thirty-two')
+
+const secret = b32.encode('12345678901234567890')
+const f = new Flowtp(secret)
+
+test('correct TOTP code for secret and time', function() {
+  expect(
+    f.at(1111111109)
+  ).toBe('081804')
+})
+
+test('verifies that a code is correct for a certain time', function() {
+  expect(
+    f.verifyAt('081804', 1111111109)
+  ).toBeTruthy()
+})
