d9b87e67b34355d3f27f5b32d05abd344cc2a10e
Braden Jonsson <braden337@gmail.com>
2017-12-15 07:30:45 -0600

    changed folder structure a little

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # Flowtp
+
+This is a JavaScript class and a command line tool that helps you process and
+manage Time-Base One Time Passwords (TOTP) that are used for a second factor of
+authentication in some applications.
+
+Implementing this has helped me better understand bit manipilation.
+
+## Implementation
+
+- follows [RFC6238](https://tools.ietf.org/html/rfc6238)
+- uses a Unix time step of 30 seconds
+- uses SHA1 in the process of calculating the TOTP
+
+## Library
+
+```javascript
+const Flowtp = require('flowtp')
+
+// at this moment, Unix time is 1513304902 so
+let now = 1513304902
+
+// create an instance of Flotp
+// passing it a base32 encoded secret
+let linda = new Flowtp('HELLOWORLD234567')
+
+// if you don't pass it a secret,
+// the constructor will generate a
+// secret for you and set it as an
+// instance variable
+let paul = new Flowtp()
+
+paul.secret // 'L6QVQRCVFXXP2T2G'
+paul.now()  // '665713'
+
+linda.now()       // '999328'
+linda.at(now+30)  // '184569'
+
+linda.verify('999328')           // true
+linda.verifyAt('184569', now+30) // true
+
+linda.toString() // '999 328'
+```
--- index.js
+++ index.js
@@ -1,27 +1,72 @@
 #!/usr/bin/env node
 
-const Flowtp = require('./src/flowtp')
+const path = require('path')
 const fs = require('fs')
-const accountsFile = './data/accounts.json'
-let accounts = require(accountsFile)
+const os = require('os')
 
-let a = process.argv[2]
+const Flowtp = require('./src/flowtp')
+
+const flowdir = path.join(os.homedir(), '.config/flowtp/')
+const accountsFile = path.join(flowdir, 'accounts.json')
+
+function writeEmptyJSON(dir) {
+  fs.writeFileSync(dir, JSON.stringify({}))
+}
+
+// This could be moved to a postinstall script
+try {
+  fs.mkdirSync(flowdir) // throws error if folder already exists
+  writeEmptyJSON(accountsFile) // write empty file if the folder was just created
+} catch (e) {
+  try {
+    fs.statSync(accountsFile) // throws error if file doesn't exist
+  } catch (e) {
+    writeEmptyJSON(accountsFile) // write empty file when one doesn't exist
+  }
+}
+
+let accounts = require(accountsFile) || {}
+
+let command = process.argv[2]
 let name = process.argv[3]
 let secret = process.argv[4]
 
-if (a == 'add' && name && secret) {
-  accounts[name] = secret
-  fs.writeFileSync(accountsFile, JSON.stringify(accounts, null, 2))
-  console.log(name, 'was added to the json file')
+if (command == 'add') {
+  if (name && secret) {
+    accounts[name] = secret
+    fs.writeFileSync(accountsFile, JSON.stringify(accounts, null, 2))
+    console.log(name, 'was added to the JSON file')
+  } else {
+    console.log('Usage: flowtp add <account> <secret>')
+  }
   process.exit(0)
 }
 
-if (a && accounts[a]) {
+if (command == 'remove') {
+  if (name) {
+    delete accounts[name]
+    fs.writeFileSync(accountsFile, JSON.stringify(accounts, null, 2))
+    console.log(name, 'was removed from the JSON file')
+  } else {
+    console.log('Usage: flowtp remove <account>')
+  }
+  process.exit(0)
+}
+
+if (command == 'list') {
+  console.log(JSON.stringify(accounts, null, 2))
+  process.exit(0)
+}
+
+if (command && accounts[command]) {
   console.log(new Flowtp(accounts[process.argv[2]]).toString())
-} else if (a && !accounts[a]) {
-  console.log(a, 'was not in the json file')
-  console.log('add it with the command \'flowtp add', a, '<secret>\'')
+} else if (command && !accounts[command]) {
+  console.log(command, 'was not in the JSON file')
+  console.log('add it with the command \'flowtp add', command, '<secret>\'')
 } else {
   console.log('Usage: flowtp <account>')
   console.log('       flowtp add <account> <secret>')
+  console.log('       flowtp remove <account>')
 }
+
+process.exit(0)
